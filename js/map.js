6f47b37d787d1ce6bbae9ceac22f07a308bea438
Va-leri <v.budilova@mail.ru>
2017-04-11 10:24:55 +0300

    Небольшие правки

--- js/map.js
+++ js/map.js
@@ -1,14 +1,18 @@
 // js/map.js
 'use strict';
+var getRandomInteger = function(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 var offerTitles = [
-  "Большая уютная квартира", 
-  "Маленькая неуютная квартира", 
-  "Огромный прекрасный дворец", 
-  "Маленький ужасный дворец", 
-  "Красивый гостевой домик", 
-  "Некрасивый негостеприимный домик", 
-  "Уютное бунгало далеко от моря", 
-  "Неуютное бунгало по колено в воде"
+  'Большая уютная квартира', 
+  'Маленькая неуютная квартира', 
+  'Огромный прекрасный дворец', 
+  'Маленький ужасный дворец', 
+  'Красивый гостевой домик', 
+  'Некрасивый негостеприимный домик', 
+  'Уютное бунгало далеко от моря', 
+  'Неуютное бунгало по колено в воде'
 ];
 var offerTypes = [
   'flat',
@@ -24,9 +28,18 @@ var offerCheckins = [
 
 var offerCheckouts = offerCheckins;
 
+var offerFeatures = [
+  'wifi', 
+  'dishwasher', 
+  'parking', 
+  'washer', 
+  'elevator', 
+  'conditioner'
+]
+
 var appartments = [];
 
-for (var i = 0, i <= offerTitles.length, i++) {
+for (var i = 0; i < offerTitles.length; i++) {
   appartments[i] = {
     'author' : {
       'avatar' : 'img/avatars/user{{0' + (i+1) + '}}.png'
@@ -34,18 +47,19 @@ for (var i = 0, i <= offerTitles.length, i++) {
     'offer' : {
       'title' : offerTitles[i],
       'address' : '{{location.x}}, {{location.y}}',
-      'price' : Math.floor((1 - Math.random()) * 1000000),
-      'type' : 5,
-      'rooms' : Math.floor((1 - Math.random()) * 5),
+      'price' : getRandomInteger(1000, 1000000),
+      'type' : offerTypes[getRandomInteger(0, 2)],
+      'rooms' : getRandomInteger(1, 5),
       'guests' : Math.floor((1 - Math.random()) * 100),
-      'checkin' : offerCheckins[Math.floor((1 - Math.random()) * 3)],
-      'checkout' : offerCheckouts[Math.floor((1 - Math.random()) * 3)],
+      'checkin' : offerCheckins[Math.floor(Math.random() * 3)],
+      'checkout' : offerCheckouts[Math.floor(Math.random() * 3)],
       'features' : '',
       'description' : '',
       'photos' : []
     },
     'location' : {
-      'x' :
+      'x' : 5,
+      'y' : 5
     }
   }
 }
